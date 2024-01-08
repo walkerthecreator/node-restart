bd62dab1355b63abe5eafd806f2aae6c24799375
walkerthecreator <nitinjaswal2616@gmail.com>
2024-01-08 16:10:23 +0530

    partial

--- controller/blog.js
+++ controller/blog.js
@@ -6,6 +6,11 @@ async function getBlogs(req , res){
     res.render('blogs' , { blog : blogs })
 }
 
+async function deleteBlog(req , res){
+    const id = req.params.id
+    const Blogstatus = await Blog.deleteOne({ _id : id })
+    res.redirect('/blog')
+}
 
 function getAddNewBlog(req , res){
     res.status(200).render('add_new_blog')
@@ -17,4 +22,4 @@ async function postAddNewBlog(req , res){
     res.redirect('/blog')
 }
 
-module.exports = { getAddNewBlog , postAddNewBlog , getBlogs}
+module.exports = { getAddNewBlog , postAddNewBlog , getBlogs , deleteBlog}
--- routes/blog.js
+++ routes/blog.js
@@ -1,5 +1,5 @@
 const express = require("express")
-const { getAddNewBlog, postAddNewBlog, getBlogs } = require("../controller/blog")
+const { getAddNewBlog, postAddNewBlog, getBlogs , deleteBlog } = require("../controller/blog")
 const router = express.Router()
 
 
@@ -9,6 +9,11 @@ router.get("/" , getBlogs)
 // @path -> http://localhost:4000/blog/add-new-blog
 router.get("/add-new-blog" , getAddNewBlog)
 router.post('/add-new-blog' , postAddNewBlog)
+
+// @method delete
+// @ path -> http://localhost:4000/blog/delete/id
+
+router.get("/delete/:id" , deleteBlog)
 
 
 // router.route("/add-new-blog").get(getAddNewBlog).post(postAddNewBlog)
